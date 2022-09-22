d51ff42584f4be5148ea8d42b5049bd43654d54a
rkisztian <radeczki.krisztian@gmail.com>
2022-09-22 09:53:19 +0200

    Hiányos, de még befejezem.

--- src/hu/petrik/OOPalapok/Main.java
+++ src/hu/petrik/OOPalapok/Main.java
@@ -9,6 +9,9 @@ public class Main {
         Kor k = new Kor();
         Kor k2 = new Kor(10);
         Kor k3 = new Kor(4,5,7);
+        Kor k4 = new Kor(33,3,70);
+        Kor k5 = new Kor(34,55,43);
+        Kor k6 = new Kor(-13,90,50);
         System.out.printf("%s-nek a Kerülete: %.3f\n", k2, k2.getKerulet());
         System.out.printf("%s-nek a Területe: %.3f\n", k2, k2.getKerulet());
 
